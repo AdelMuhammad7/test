beb5d036e163bd8f5a0d75ec848abb3efc264a1a
Adel <dolaadel574@gmail.com>
2025-03-18 22:38:01 +0200

    Form

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ function hello(){
     text.style.color = "green"
     text.style.fontSize = "50px"
 }
+
+const form = document.querySelector("form")
+form.onsubmit = (e)=> {
+    e.preventDefault()
+    document.querySelectorAll("input").forEach((el) => {
+        console.log(el.value)
+    })
+    
+}
